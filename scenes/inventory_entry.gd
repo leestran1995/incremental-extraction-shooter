8786fd59af8c1e34a5768c48eb9e3cbd4b8e477a
Lee Tran <leestran1995@gmail.com>
2026-01-07 17:09:25 -0600

    implement inventory popup and right click to remove from inventory

--- scenes/inventory_entry.gd
+++ scenes/inventory_entry.gd
@@ -1,6 +1,17 @@
 extends Control
 @onready var inventory_entry_texture_rect: TextureRect = %InventoryEntryTextureRect
 @onready var inventory_entry_color_rect: ColorRect = %InventoryEntryColorRect
+@onready var panel_container: PanelContainer = $PanelContainer
+@onready var entry_name_popup_label: Label = %EntryNamePopupLabel
+@onready var entry_name_popup: PanelContainer = %EntryNamePopup
+
+var mouse_hovering: bool = false:
+	set(new_value):
+		mouse_hovering = new_value
+		if mouse_hovering:
+			entry_name_popup.show()
+		else:
+			entry_name_popup.hide()
 
 var image_path: String:
 	set(new_value):
@@ -9,26 +20,40 @@ var image_path: String:
 		image_path = new_value
 		var loaded = load(image_path)
 		inventory_entry_texture_rect.texture = loaded
+		
+var item_name: String:
+	set(new_value):
+		item_name = new_value
+		entry_name_popup_label.text = item_name
+
+var invy_index: int
+
+var right_click_down: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	entry_name_popup.hide()
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		print(event)
-		
-
+	
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		print(event)
-
-
+	if mouse_hovering:
+		if event is InputEventMouseButton:
+			if not right_click_down:
+				right_click_down = true
+				return
+				
+			if event.button_index == 2:
+				PlayerInventory.remove_item(invy_index)
+		
 func _on_mouse_entered() -> void:
-	print("mouse entered")
-	pass # Replace with function body.
+	if not mouse_hovering:
+		mouse_hovering = true
+
+func _on_mouse_exited() -> void:
+	right_click_down = false
+	if mouse_hovering:
+		mouse_hovering = false
--- scenes/inventory_scene.gd
+++ scenes/inventory_scene.gd
@@ -2,26 +2,38 @@ extends Control
 @onready var grid_container: GridContainer = $GridContainer
 const INVENTORY_ENTRY = preload("uid://ddsn42ctjj33v")
 
-
+var should_redraw: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	PlayerInventory.player_inventory_updated.connect(set_redraw)
 	pass # Replace with function body.
+
+func set_redraw():
+	should_redraw = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if not should_redraw:
+		return
+	
 	for n in grid_container.get_children():
 		n.queue_free()
 
 	var items = PlayerInventory.items
+	var index = 0
 	
 	for i in items:
+		print("drawing item")
 		var new_entry = INVENTORY_ENTRY.instantiate()
-		var control_node = new_entry.get_child(0)
 		grid_container.add_child(new_entry)
 
 		var image_path = ItemHandler.get_image_for_item(i)
 		new_entry.image_path = image_path
+		new_entry.item_name = i
+		new_entry.invy_index = index
+		index += 1
+		
+	should_redraw = false
 
 func _on_mouse_entered() -> void:
-	print("mouse entered inventory scene")
 	pass # Replace with function body.
